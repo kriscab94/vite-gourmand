8e05e7d249a17cd14a88ccb608c8f3f82913d885
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 11:38:37 +0100

    Fix require_once statement formatting in employees.php

--- vite-gourmand/public/admin/employees.php
+++ vite-gourmand/public/admin/employees.php
@@ -1,6 +1,7 @@
 <?php
-require_once("../../config/database.php");
 session_start();
+require_once("../../config/database.php");
+
 
 if (!isset($_SESSION["user"]) || $_SESSION["user"]["role"] !== "admin") {
     die("Accès interdit");
