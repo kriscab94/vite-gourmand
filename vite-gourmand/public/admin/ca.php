692dc227e26d68ba2b2a33e470c2a1803ef77dc7
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 11:37:39 +0100

    Remove unnecessary newline in ca.php

--- vite-gourmand/public/admin/ca.php
+++ vite-gourmand/public/admin/ca.php
@@ -1,7 +1,6 @@
 <?php
-require_once("../../config/database.php");
 session_start();
-
+require_once("../../config/database.php");
 if (!isset($_SESSION["user"]) || $_SESSION["user"]["role"] !== "admin") {
     die("Accès interdit");
 }
