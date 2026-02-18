6ea2550e31e859389706c97a74cf006c699461ec
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 13:53:16 +0100

    Refactor footer.php for better structure and links

--- vite-gourmand/views/footer.php
+++ vite-gourmand/views/footer.php
@@ -33,9 +33,10 @@ $horaires = $stmtH->fetchAll(PDO::FETCH_ASSOC);
 
       <div class="col-md-4 mb-3">
         <h5>Liens</h5>
-        <a class="text-light d-block" href="<?= BASE_URL ?>/mentions.php">Mentions légales</a>
-        <a class="text-light d-block" href="<?= BASE_URL ?>/cgv.php">CGV</a>
-        <a class="text-light d-block" href="<?= BASE_URL ?>/contact.php">Contact</a>
+        <a class="text-light d-block" href="<?= BASE_URL ?>/mentions-legales.php">Mentions légales</a>
+<a class="text-light d-block" href="<?= BASE_URL ?>/cgv.php">CGV</a>
+<a class="text-light d-block" href="<?= BASE_URL ?>/contact.php">Contact</a>
+
       </div>
 
     </div>
@@ -44,3 +45,4 @@ $horaires = $stmtH->fetchAll(PDO::FETCH_ASSOC);
     <p class="mb-0 text-center">&copy; <?= date("Y"); ?> Vite & Gourmand</p>
   </div>
 </footer>
+
