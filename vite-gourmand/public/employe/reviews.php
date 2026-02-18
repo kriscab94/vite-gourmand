88ee5c802d935842f20254f8ca7fe7d413a197b3
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 11:42:07 +0100

    Fix PHP syntax and improve session handling

--- vite-gourmand/public/employe/reviews.php
+++ vite-gourmand/public/employe/reviews.php
@@ -1,6 +1,6 @@
 <?php
-require_once("../../config/database.php");
 session_start();
+require_once("../../config/database.php");
 
 if (!isset($_SESSION["user"]) || 
    ($_SESSION["user"]["role"] !== "employe" && $_SESSION["user"]["role"] !== "admin")) {
@@ -64,3 +64,4 @@ $avis = $stmt->fetchAll(PDO::FETCH_ASSOC);
 <?php require_once("../../views/footer.php"); ?>
 </body>
 </html>
+
