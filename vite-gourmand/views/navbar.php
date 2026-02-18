727cc3177f588c5e05f1bc2b6f7023b69ed8416a
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 12:59:48 +0100

    Replace BASE_URL with absolute paths in navbar

--- vite-gourmand/views/navbar.php
+++ vite-gourmand/views/navbar.php
@@ -1,34 +1,33 @@
-<?php require_once(__DIR__ . "/../config/app.php"); ?>
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container">
-    <a class="navbar-brand" href="<?= BASE_URL ?>/index.php">Vite & Gourmand</a>
+
+    <a class="navbar-brand" href="/index.php">Vite & Gourmand</a>
 
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav">
       <span class="navbar-toggler-icon"></span>
     </button>
 
     <div class="collapse navbar-collapse" id="nav">
+
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link" href="<?= BASE_URL ?>/index.php">Menus</a>
+          <a class="nav-link" href="/index.php">Menus</a>
         </li>
+
         <li class="nav-item">
-          <a class="nav-link" href="<?= BASE_URL ?>/contact.php">Contact</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= BASE_URL ?>/mentions-legales.php">Mentions légales</a>
+          <a class="nav-link" href="/contact.php">Contact</a>
         </li>
       </ul>
 
-      <ul class="navbar-nav ms-auto">
+      <ul class="navbar-nav">
         <?php if (!isset($_SESSION["user"])): ?>
 
           <li class="nav-item">
-            <a class="nav-link" href="<?= BASE_URL ?>/auth/login.php">Connexion</a>
+            <a class="nav-link" href="/auth/login.php">Connexion</a>
           </li>
+
           <li class="nav-item">
-            <a class="nav-link" href="<?= BASE_URL ?>/auth/register.php">Inscription</a>
+            <a class="nav-link" href="/auth/register.php">Inscription</a>
           </li>
 
         <?php else: ?>
@@ -41,26 +40,28 @@
           </li>
 
           <li class="nav-item">
-            <a class="nav-link" href="<?= BASE_URL ?>/account.php">Mon compte</a>
+            <a class="nav-link" href="/account.php">Mon compte</a>
           </li>
 
           <?php if ($_SESSION["user"]["role"] === "admin"): ?>
             <li class="nav-item">
-              <a class="nav-link" href="<?= BASE_URL ?>/admin/stats.php">Stats Mongo</a>
+              <a class="nav-link" href="/admin/stats.php">Stats Mongo</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="<?= BASE_URL ?>/admin/employees.php">Employés</a>
+              <a class="nav-link" href="/admin/ca.php">Chiffre d'affaires</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/admin/employees.php">Employés</a>
             </li>
           <?php endif; ?>
 
           <li class="nav-item">
-            <a class="nav-link" href="<?= BASE_URL ?>/auth/logout.php">Déconnexion</a>
+            <a class="nav-link" href="/auth/logout.php">Déconnexion</a>
           </li>
 
         <?php endif; ?>
       </ul>
+
     </div>
   </div>
 </nav>
-
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
