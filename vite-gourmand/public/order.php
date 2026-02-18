9135b19f61874a1774bfe9d913c1db52de1322d7
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 08:03:16 +0100

    Fix require_once statement in order.php

--- vite-gourmand/public/order.php
+++ vite-gourmand/public/order.php
@@ -1,6 +1,6 @@
 <?php
-require_once("../config/database.php");
 session_start();
+require_once("../config/database.php");
 
 if (!isset($_SESSION["user"])) {
     header("Location: auth/login.php");
