bae38eef984f521084c858c6540d11daa68ed672
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 13:07:42 +0100

    Change BASE_URL definition to an empty string

--- vite-gourmand/config/app.php
+++ vite-gourmand/config/app.php
@@ -4,4 +4,4 @@ $host = $_SERVER["HTTP_HOST"] ?? "";
 // Local si on est sur localhost / 127.0.0.1
 $isLocal = ($host === "localhost" || str_starts_with($host, "127.0.0.1") || str_contains($host, "localhost"));
 
-define("BASE_URL", $isLocal ? "/vite-gourmand/public" : "");
+define('BASE_URL', '');
