795fe17cb29622e7ee39e1a4d63385d6b36c06ad
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 13:08:54 +0100

    Add BASE_URL definition in app.php

--- vite-gourmand/config/app.php
+++ vite-gourmand/config/app.php
@@ -4,4 +4,6 @@ $host = $_SERVER["HTTP_HOST"] ?? "";
 // Local si on est sur localhost / 127.0.0.1
 $isLocal = ($host === "localhost" || str_starts_with($host, "127.0.0.1") || str_contains($host, "localhost"));
 
+<?php
 define('BASE_URL', '');
+
