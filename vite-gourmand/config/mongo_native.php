29d43dc50d85a81b48912c319dc31f7408b4f421
kriscab94 <kris.cabanis2@gmail.com>
2026-02-18 16:06:20 +0100

    Enhance mongoUri retrieval with getenv fallback

--- vite-gourmand/config/mongo_native.php
+++ vite-gourmand/config/mongo_native.php
@@ -1,8 +1,9 @@
 <?php
 
-$mongoUri = $_ENV["MONGO_URL"] ?? "mongodb://127.0.0.1:27017";
+$mongoUri = $_ENV["MONGO_URL"] ?? getenv("MONGO_URL") ?? "mongodb://127.0.0.1:27017";
 
 $mongoManager = new MongoDB\Driver\Manager($mongoUri);
 
 $mongoDbName = "vite_gourmand_stats";
 $mongoCollection = "commandes_stats";
+
